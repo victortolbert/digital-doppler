0f47dd29f233f227e1ca221de754a4be4c48e0e9
Victor Tolbert <victor.tolbert@gmail.com>
2024-07-16 07:23:12 -0400

    feature: add official integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
-
+import vue from "@astrojs/vue";
+import alpinejs from "@astrojs/alpinejs";
+import svelte from "@astrojs/svelte";
+import netlify from "@astrojs/netlify";
+import db from "@astrojs/db";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+
+import tunnel from "astro-tunnel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [vue(), alpinejs(), svelte(), db(), mdx(), partytown(), tailwind(), sitemap(), react(), tunnel()],
+  output: "server",
+  adapter: netlify()
 });
